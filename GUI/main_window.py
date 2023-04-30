d4e592e3332a66bb2382eea55efdf5b60477bdfd
aydachettit <dantetran04@gmail.com>
2023-04-30 18:11:50 +0700

    nam

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1003,7 +1003,7 @@ class Ui_MainWindow(object):
 
 
         self.retranslateUi(MainWindow)
-        self.stackedWidget.setCurrentIndex(2)
+        self.stackedWidget.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
